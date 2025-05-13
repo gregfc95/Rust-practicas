1104f4497b3fac5c083c8a5013581d1ce3b72598
gregfc95 <josegfc95@gmail.com>
2025-05-12 22:08:40 -0300

    feat: add test ej8

--- p3/src/tp3/ej8.rs
+++ p3/src/tp3/ej8.rs
@@ -20,7 +20,7 @@ pub enum Genero {
     Jazz,
     Otros,
 }
-
+#[derive(Debug, PartialEq, Clone)]
 pub struct Cancion {
     titulo: String,
     artista: String,
@@ -33,7 +33,7 @@ pub struct Playlist {
 }
 
 impl Playlist {
-    pub fn new(nombre: String) -> Self {
+    pub fn new(nombre: String, canciones: Vec<Cancion>) -> Self {
         Playlist {
             nombre,
             canciones: Vec::new(),
@@ -110,5 +110,173 @@ impl Cancion {
             artista,
             genero,
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::vec;
+
+    use super::*;
+
+    #[test]
+    fn test_crear_cancion() {
+        let cancion = Cancion::new("Cancion".to_string(), "Artista".to_string(), Genero::Rock);
+        assert_eq!(cancion.titulo, "Cancion");
+        assert_eq!(cancion.artista, "Artista");
+        assert_eq!(cancion.genero, Genero::Rock);
+    }
+
+    #[test]
+    fn test_set_nombre_playlist() {
+        let mut playlist = Playlist::new("Mi playlist".to_string(), vec![]);
+        let nuevo_nombre = "Mi nueva playlist".to_string();
+        playlist.set_nombre(nuevo_nombre);
+        assert_eq!(playlist.nombre, "Mi nueva playlist");
+    }
+
+    #[test]
+    fn test_agregar_cancion() {
+        let mut playlist = Playlist::new("Mi playlist".to_string(), vec![]);
+        let cancion = Cancion::new("Cancion".to_string(), "Artista".to_string(), Genero::Rock);
+        playlist.agregar_cancion(cancion);
+        assert_eq!(playlist.canciones.len(), 1);
+    }
+    #[test]
+    fn test_eliminar_canciones() {
+        let mut playlist = Playlist::new("Mi playlist".to_string(), vec![]);
+        let cancion = Cancion::new("Cancion".to_string(), "Artista".to_string(), Genero::Rock);
+        playlist.agregar_cancion(cancion);
+        assert_eq!(playlist.canciones.len(), 1);
+        playlist.eliminar_canciones();
+        assert_eq!(playlist.canciones.len(), 0);
+    }
+
+    #[test]
+    fn test_buscar_cancion_por_nombre() {
+        let mut playlist = Playlist::new("Mi playlist".to_string(), vec![]);
+        let cancion = Cancion::new("Cancion".to_string(), "Artista".to_string(), Genero::Rock);
+        playlist.agregar_cancion(cancion);
+        assert_eq!(playlist.canciones.len(), 1);
+        assert!(playlist.buscar_cancion_por_nombre("Cancion").is_some());
+    }
+
+    #[test]
+    fn test_buscar_cancion_por_nombre_no_existe() {
+        let mut playlist = Playlist::new("Mi playlist".to_string(), vec![]);
+        let cancion = Cancion::new("Cancion".to_string(), "Artista".to_string(), Genero::Rock);
+        playlist.agregar_cancion(cancion);
+        assert_eq!(playlist.canciones.len(), 1);
+        assert!(playlist.buscar_cancion_por_nombre("NoExiste").is_none());
+    }
+
+    #[test]
+    fn test_buscar_cancion_por_genero() {
+        let mut playlist = Playlist::new("Mi playlist".to_string(), vec![]);
+        let cancion_rock = Cancion::new(
+            "CancionRock".to_string(),
+            "Artista".to_string(),
+            Genero::Rock,
+        );
+        let cancion_pop =
+            Cancion::new("CancionPop".to_string(), "Artista".to_string(), Genero::Pop);
+        let cancion_jazz = Cancion::new(
+            "CancionJazz".to_string(),
+            "Artista".to_string(),
+            Genero::Jazz,
+        );
+        playlist.agregar_cancion(cancion_rock);
+        playlist.agregar_cancion(cancion_pop);
+        playlist.agregar_cancion(cancion_jazz);
+        let vec_rock: Vec<&Cancion> = playlist.obtener_canciones_por_genero(Genero::Rock);
+        let vec_otros: Vec<&Cancion> = playlist.obtener_canciones_por_genero(Genero::Otros);
+        assert_eq!(vec_rock.len(), 1);
+        assert_eq!(vec_otros.len(), 0);
+    }
+
+    #[test]
+    fn test_buscar_cancion_por_artista() {
+        let mut playlist = Playlist::new("Mi playlist".to_string(), vec![]);
+        let cancion_rock = Cancion::new(
+            "CancionRock".to_string(),
+            "ArtistaRock".to_string(),
+            Genero::Rock,
+        );
+        let cancion_pop = Cancion::new(
+            "CancionPop".to_string(),
+            "ArtistaPop".to_string(),
+            Genero::Pop,
+        );
+        let cancion_jazz = Cancion::new(
+            "CancionJazz".to_string(),
+            "ArtistaJazz".to_string(),
+            Genero::Jazz,
+        );
+        playlist.agregar_cancion(cancion_rock);
+        playlist.agregar_cancion(cancion_pop);
+        playlist.agregar_cancion(cancion_jazz);
+        let vec_rock: Vec<&Cancion> = playlist.obtener_canciones_por_artista("ArtistaRock");
+        let vec_otros: Vec<&Cancion> = playlist.obtener_canciones_por_artista("ArtistaOtros");
+        assert_eq!(vec_rock.len(), 1);
+        assert_eq!(vec_otros.len(), 0);
+    }
+
+    #[test]
+    fn test_eliminar_cancion() {
+        let mut playlist = Playlist::new("Mi playlist".to_string(), vec![]);
+        let cancion = Cancion::new("Cancion".to_string(), "Artista".to_string(), Genero::Rock);
+        playlist.agregar_cancion(cancion.clone());
+        assert_eq!(playlist.canciones.len(), 1);
+        assert!(playlist.eliminar_cancion(&cancion).is_some());
+        assert_eq!(playlist.canciones.len(), 0);
+    }
+
+    #[test]
+    fn test_eliminar_cancion_no_existe() {
+        let mut playlist = Playlist::new("Mi playlist".to_string(), vec![]);
+        let cancion = Cancion::new("Cancion".to_string(), "Artista".to_string(), Genero::Rock);
+        let cancion_jazz = Cancion::new(
+            "CancionJazz".to_string(),
+            "ArtistaJazz".to_string(),
+            Genero::Jazz,
+        );
+        playlist.agregar_cancion(cancion.clone());
+        assert_eq!(playlist.canciones.len(), 1);
+        assert!(playlist.eliminar_cancion(&cancion_jazz).is_none());
+        assert_eq!(playlist.canciones.len(), 1);
+    }
+
+    #[test]
+    fn test_mover_ultima_cancion_al_inicio() {
+        let mut playlist = Playlist::new("Mi playlist".to_string(), vec![]);
+        let a = Cancion::new("A".to_string(), "ArtistaRock".to_string(), Genero::Rock);
+        let b = Cancion::new("B".to_string(), "ArtistaPop".to_string(), Genero::Pop);
+        let c = Cancion::new("C".to_string(), "ArtistaJazz".to_string(), Genero::Jazz);
+
+        playlist.agregar_cancion(a);
+        playlist.agregar_cancion(b);
+        playlist.agregar_cancion(c.clone());
+
+        assert!(playlist.mover_cancion(&c, 0));
+        assert_eq!(playlist.canciones[0].titulo, "C");
+        assert_eq!(playlist.canciones[1].titulo, "A");
+        assert_eq!(playlist.canciones[2].titulo, "B");
+    }
+
+    #[test]
+    fn test_mover_cancion_posicion_invalida() {
+        let mut playlist = Playlist::new("Mi playlist".to_string(), vec![]);
+        let a = Cancion::new("A".to_string(), "ArtistaRock".to_string(), Genero::Rock);
+        let b = Cancion::new("B".to_string(), "ArtistaPop".to_string(), Genero::Pop);
+        let c = Cancion::new("C".to_string(), "ArtistaJazz".to_string(), Genero::Jazz);
+
+        playlist.agregar_cancion(a);
+        playlist.agregar_cancion(b);
+        playlist.agregar_cancion(c.clone());
+
+        assert_eq!(playlist.mover_cancion(&c, 10), false);
+        assert_eq!(playlist.canciones[0].titulo, "A");
+        assert_eq!(playlist.canciones[1].titulo, "B");
+        assert_eq!(playlist.canciones[2].titulo, "C");
     }
 }
